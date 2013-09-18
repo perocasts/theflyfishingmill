814562d1f1c55dd03c0038d94bb9d56bb878fe88
Peter Berkenbosch <peter@pero-ict.nl>
2013-09-18 09:24:34 +0200

    add expectation about the page title.

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,6 +3,7 @@ require "spec_helper"
 describe "Homepage" do
   it "will render the homepage" do
     visit "/"
-    page.should have_content "No products found"
+    expect(page).to have_content "No products found"
+    expect(page).to have_title "The FlyfishingMill Store"
   end
 end
