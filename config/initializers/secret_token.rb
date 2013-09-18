c13c1a431e38df55398f611308e7ae28f692e440
Peter Berkenbosch <peter@pero-ict.nl>
2013-09-18 08:53:48 +0200

    generated rails 4 app skeleton, without testing (will be added later) and with postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Theflyfishingmill::Application.config.secret_key_base = '4568798626b74f8e9698217f3fc3c770625991e84078ca32f47e70e60f00623b7108e6f748493830c587fe35370f9948d01ff1f017408d78eee41fdf10543182'
