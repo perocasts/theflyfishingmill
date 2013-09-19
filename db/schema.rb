978e0115bef7713570001ea2159d24ffa5febb71
Peter Berkenbosch <peter@pero-ict.nl>
2013-09-19 09:41:43 +0200

    updated to latest spree and spree_auth_devise installed new migrations and ran them.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130918070525) do
+ActiveRecord::Schema.define(version: 20130919074126) do
 
   create_table "spree_activators", force: true do |t|
     t.string   "description"
